549250e9cad7f7b29df35ecb7742da1f333fe579
Antony Omeri <antonyomeri@gmail.com>
2017-01-05 01:55:11 +0300

    Added payment bridge portal

---
+++ payment-bridge-portal/src/main/java/com/omexit/malipo/paymentbridgeportal/PaymentBridgePortalApplication.java
@@ -0,0 +1,12 @@
+package com.omexit.malipo.paymentbridgeportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentBridgePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentBridgePortalApplication.class, args);
+	}
+}
