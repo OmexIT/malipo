9d0035c6f101a1dcaf015b2d6b89d334f6cc2494
Antony Omeri <antonyomeri@gmail.com>
2017-01-05 01:46:23 +0300

    Added integration context

--- payment-bridge/src/main/java/com/omexit/malipo/paymentbridge/PaymentBridgeApplication.java
+++ payment-bridge/src/main/java/com/omexit/malipo/paymentbridge/PaymentBridgeApplication.java
@@ -2,8 +2,10 @@ package com.omexit.malipo.paymentbridge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:integration-context.xml")
 public class PaymentBridgeApplication {
 
 	public static void main(String[] args) {
